53652161c5b3aba1e248348748d1fb5bc5780e14
Rinnegatamante <rinnegatamante@gmail.com>
2018-03-03 18:12:07 +0100

    Fixed Profile page showing supporters as guests.

--- user/profile.controller.js
+++ user/profile.controller.js
@@ -39,6 +39,9 @@ app.controller('profileController',function ($scope, $rootScope, $http, $locatio
 			}else if (res.data[0].roles[0] == "5"){
 				$scope.conf.role = "User"
 				$scope.conf.color = "blue"
+			}else if (res.data[0].roles[0] == "6"){
+				$scope.conf.role = "Supporter"
+				$scope.conf.color = "Tomato"
 			}else{
 				$scope.conf.role = "Guest"
 				$scope.conf.color = "black"
